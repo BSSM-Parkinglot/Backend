1e7c488b782fbe2066505be809c4d8bd46d6fc05
anys34 <anyeseong34@gmail.com>
2023-07-18 15:04:25 +0900

    ADD :: create tables

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const Sequelize = require("sequelize");
 const User = require("./user");
+const Spot_Info = require("./spot_info");
+const Car_Info = require("./car_info");
 
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config")[env];
@@ -15,9 +17,15 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 
 db.User = User;
+db.Spot_Info = Spot_Info;
+db.Car_Info = Car_Info;
 
 User.initiate(sequelize);
+Spot_Info.initiate(sequelize);
+Car_Info.initiate(sequelize);
 
 User.associate(db);
+Spot_Info.associate(db);
+Car_Info.associate(db);
 
 module.exports = db;
--- models/user.js
+++ models/user.js
@@ -4,22 +4,23 @@ class User extends Sequelize.Model {
   static initiate(sequelize) {
     User.init(
       {
+        Id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
         UserName: {
-          type: Sequelize.STRING(20),
-          unique: true,
-          allowNull: false,
+            type: Sequelize.STRING(50),
         },
         Password: {
-          type: Sequelize.STRING(20),
-          allowNull: false,
+          type: Sequelize.STRING(255),
         },
         CarNum: {
           type: Sequelize.STRING(20),
-          allowNull: false,
+          unique: true,
         },
         Enter: {
-          type: Sequelize.STRING(20),
-          allowNull: false,
+          type: Sequelize.BOOLEAN,
         },
       },
       {
@@ -27,14 +28,19 @@ class User extends Sequelize.Model {
         timestamps: false,
         underscored: false,
         modelName: "User",
-        tableName: "users",
+        tableName: "User",
         paranoid: false,
         charset: "utf8",
         collate: "utf8_general_ci",
       }
     );
   }
-  static associate(db) {}
+
+  static associate(db) {
+    db.User.hasOne(db.Car_Info, {
+        foreignKey: "CarNum"
+      });
+  }
 }
 
 module.exports = User;
