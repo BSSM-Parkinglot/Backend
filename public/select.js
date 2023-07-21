097f50d0974993dec5052227e760e4ac6f8964c3
anys34 <anyeseong34@gmail.com>
2023-07-21 19:32:05 +0900

    ADD :: 회원가입

--- public/select.js
+++ public/select.js
@@ -1,14 +1,26 @@
 document.getElementById("car-form").addEventListener("submit", async (e) => {
   e.preventDefault();
   const carnum = e.target.carnum.value;
-  if (!carnum) {
-    return alert("자동차 번호를 입력하세요");
-  }
 
   try {
     await axios.post("/car", { carnum });
   } catch (err) {
     console.error(err);
   }
+  e.target.carnum.value = "";
+});
+
+document.getElementById("signup-form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = e.target.username.value;
+  const password = e.target.password.value;
+  const carnum = e.target.carnum.value;
+  try {
+    await axios.post("/signup", { username, password, carnum });
+  } catch (err) {
+    console.error(err);
+  }
+  e.target.username.value = "";
+  e.target.password.value = "";
   e.target.carnum.value = "";
 });
