5fc82a1caec936f6f5dfa798c7e202943f160641
anys34 <anyeseong34@gmail.com>
2023-07-23 02:38:07 +0900

    ADD :: 로그인

---
+++ routes/login.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  res.render("login");
+});
+
+router.post("/", async (req, res, next) => {
+  const user = await User.findAll({
+    where: {
+      UserName: req.body.username,
+      Password: req.body.password,
+    },
+  });
+
+  if (user.length > 0) {
+    console.log("로그인 성공");
+    res.send("success");
+  } else {
+    console.log("로그인 실패");
+    res.send("failure");
+  }
+});
+
+module.exports = router;
