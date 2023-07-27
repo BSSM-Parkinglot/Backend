3890b74d1fcf7a812e71dd55f696ee9fc51fbd03
anys34 <anyeseong34@gmail.com>
2023-07-27 09:49:36 +0900

    ADD :: ndoemon

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const Car_Info = require("../models/car_info");
+const Spot_Info = require("../models/spot_info");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  const cars = await Car_Info.findAll();
+  const spots = await Spot_Info.findAll();
   const user = req.session.user;
-  if (user) {
-    res.render("index", { cars, user });
-  } else {
-    res.render("index", { cars });
-  }
+  res.render("index", { spots, user });
 });
 
 module.exports = router;
